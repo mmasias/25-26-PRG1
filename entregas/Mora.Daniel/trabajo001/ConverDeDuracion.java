0cab782a76ac81181ef6ccb628b57bc46d9a16a2
ddmora07 <daniel.mora@alumnos.uneatlantico.es>
2025-10-07 16:32:05 +0200

    Completado de 2 retos

---
+++ entregas/Mora.Daniel/trabajo001/ConverDeDuracion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class ConverDeDuracion {
+    public static void main(String[] args) {
+        final int DIAS_EN_SEGUNDOS = 86400;
+        final int HORAS_EN_SEGUNDOS = 3600;
+        final int MINUTOS_EN_SEGUNDOS = 60;
+        final int SEGUNDOS = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Cuántos segundos desea convertir?");
+        int segundos = scanner.nextInt();
+        int dias = segundos / DIAS_EN_SEGUNDOS;
+        int horas = (segundos % DIAS_EN_SEGUNDOS) / HORAS_EN_SEGUNDOS;
+        int minutos = (segundos % HORAS_EN_SEGUNDOS) / MINUTOS_EN_SEGUNDOS;
+        int segundosConvertidos = (segundos % MINUTOS_EN_SEGUNDOS) / SEGUNDOS;
+        System.out.println(dias + "dia(s)");
+        System.out.println(horas + "hora(s)");
+        System.out.println(minutos + "minuto(s)");
+        System.out.println(segundosConvertidos + "segundos(s)");
+        scanner.close();
+
+    }
+}
