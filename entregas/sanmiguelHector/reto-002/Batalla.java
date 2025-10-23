7aac328477123d617c9b034e6c842239ccd202a4
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-10-23 19:41:45 +0200

    Reto extendido con elecciones

--- entregas/sanmiguelHector/reto-002/Batalla.java
+++ entregas/sanmiguelHector/reto-002/Batalla.java
@@ -1,19 +1,52 @@
+import java.util.Scanner;
+
 class Batalla {
     public static void main(String[] args) {
-        final int DAÑO_ESPADA = 2;
-        int vidaGuerrero = 20;
-        final double PORCENTAJE_EXITO_GUERRERO = 0.5;
+        final int ARMA_1 = 7;
+        final int ARMA_2 = 15;
+        final int ARMA_3 = 30;
+        final double PORCENTAJE_EXITO_ARMA_1 = 0.5;
+        final double PORCENTAJE_EXITO_ARMA_2 = 0.25;
+        final double PORCENTAJE_EXITO_ARMA_3 = 0.12;
+        int vidaGuerrero = 150;
 
-        final int DAÑO_MORDIDA = 2;
-        int vidaVampiro = 10;
-        final double PORCENTAJE_EXITO_VAMPIRO = 0.5;
+        final int ATAQUE_1 = 7;
+        final int ATAQUE_2 = 15;
+        final int ATAQUE_3 = 30;
+        final double PORCENTAJE_EXITO_ATAQUE_1 = 0.9;
+        final double PORCENTAJE_EXITO_ATAQUE_2 = 0.6;
+        final double PORCENTAJE_EXITO_ATAQUE_3 = 0.4;
+        int vidaVampiro = 60;
 
         boolean algunMuerto = false;
+        Scanner scanner = new Scanner(System.in);
 
         do {
-            if (Math.random() < PORCENTAJE_EXITO_GUERRERO) {
-                vidaVampiro -= DAÑO_ESPADA;
-                System.out.println("El vampiro recibe una hostia (-" + DAÑO_ESPADA + " vida)");
+            System.out.println("Escoge el arma de tu héroe:");
+            System.out.println("1. Arma 1: 7 puntos de daño y 50% de éxito");
+            System.out.println("2. Arma 2: 15 puntos de daño y 25% de éxito");
+            System.out.println("3. Arma 3: 30 puntos de daño y 12% de éxito");
+            int eleccion = scanner.nextInt();
+
+            int dañoGuerrero = 0;
+            double probExitoGuerrero = 0;
+
+            if (eleccion == 1) {
+                dañoGuerrero = ARMA_1;
+                probExitoGuerrero = PORCENTAJE_EXITO_ARMA_1;
+            } else if (eleccion == 2) {
+                dañoGuerrero = ARMA_2;
+                probExitoGuerrero = PORCENTAJE_EXITO_ARMA_2;
+            } else if (eleccion == 3) {
+                dañoGuerrero = ARMA_3;
+                probExitoGuerrero = PORCENTAJE_EXITO_ARMA_3;
+            } else {
+                System.out.println("Elección inválida, pierdes el turno.");
+            }
+
+            if (Math.random() < probExitoGuerrero) {
+                vidaVampiro -= dañoGuerrero;
+                System.out.println("El vampiro recibe una ostia (-" + dañoGuerrero + " vida)");
             } else {
                 System.out.println("El vampiro esquiva la hostia");
             }
@@ -24,9 +57,24 @@ class Batalla {
                 break;
             }
 
-            if (Math.random() < PORCENTAJE_EXITO_VAMPIRO) {
-                vidaGuerrero -= DAÑO_MORDIDA;
-                System.out.println("El guerrero recibe una mordida (-" + DAÑO_MORDIDA + " vida)");
+            int ataqueVampiro = (int)(Math.random() * 3) + 1;
+            int dañoVampiro = 0;
+            double probExitoVampiro = 0;
+
+            if (ataqueVampiro == 1) {
+                dañoVampiro = ATAQUE_1;
+                probExitoVampiro = PORCENTAJE_EXITO_ATAQUE_1;
+            } else if (ataqueVampiro == 2) {
+                dañoVampiro = ATAQUE_2;
+                probExitoVampiro = PORCENTAJE_EXITO_ATAQUE_2;
+            } else {
+                dañoVampiro = ATAQUE_3;
+                probExitoVampiro = PORCENTAJE_EXITO_ATAQUE_3;
+            }
+
+            if (Math.random() < probExitoVampiro) {
+                vidaGuerrero -= dañoVampiro;
+                System.out.println("El guerrero recibe una mordida (-" + dañoVampiro + " vida)");
             } else {
                 System.out.println("El guerrero esquiva la mordida");
             }
@@ -44,5 +92,6 @@ class Batalla {
         } while (!algunMuerto);
 
         System.out.println("Fin de la batalla.");
+        scanner.close();
     }
 }
