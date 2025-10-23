3adeba9cfb925185ab8953a36aeffb18ae9aa58c
Cyber <marcosbolivar2007@gmail.com>
2025-10-23 22:29:30 +0200

    reto adicional nº3 y final completado.

--- entregas/bolivarMarcos/reto002/reto002.java
+++ entregas/bolivarMarcos/reto002/reto002.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class reto002 {
     public static void main(String[] args) {
 
-        int numeroArmaEscogida;
+        int numeroArmaEscogida = 0;
         int numeroAtaqueVampiro;
-        int vidaHeroe = 160;
+        final int MAX_VIDA_HEROE = 160;
+        int vidaHeroe = MAX_VIDA_HEROE;
         final int DAÑO_HEROE_ARMA_1 = 7;
         final int DAÑO_HEROE_ARMA_2 = 15;
         final int DAÑO_HEROE_ARMA_3 = 30;
@@ -17,29 +18,59 @@ public class reto002 {
         final int VIDA_MINIMA_DESMAYO_HEROE = 20;
         boolean desmayoHeroe = false;
 
+        final int MAX_VIDA_VAMPIRO = 60;
         final int CANTIDAD_ATAQUES_VAMPIRO = 3;
         final int DAÑO_VAMPIRO_ATAQUE_1 = 5;
         final int DAÑO_VAMPIRO_ATAQUE_2 = 10;
         final int DAÑO_VAMPIRO_ATAQUE_3 = 20;
-        int vidaVampiro = 60;
+        int vidaVampiro = MAX_VIDA_VAMPIRO;
         double probabilidadExitoVampiro = 0;
         int dañoVampiro = 0;
         final int VIDA_MINIMA_DESMAYO_VAMPIRO = 20;
         boolean alguienVivo = true;
         boolean vampiroVivo = true;
         boolean desmayoVampiro = false;
+        int turno = 0;
+        boolean pocion = false;
+        boolean turnoBloqueado = false;
+        int turnoActual = 0;
+
         Scanner input = new Scanner(System.in);
 
         while (alguienVivo) {
-            System.out.println(
-                    "Con que arma quieres atacar al vampiro?\n1: (7 dmg/50% prob)\n2: (15 dmg/25% prob)\n3: (30 dmg/12% prob)\n4 (Defenderse // 0 dmg/80% prob/-5 dmg vampiro)");
-            numeroArmaEscogida = input.nextInt();
+
+            turno++;
+            System.out.println("TURNO " + turno + ":");
+
+            if (turno == 3) {
+                pocion = true;
+                System.out.println("Se ha desbloqueado la poción mística");
+            }
+            if (!turnoBloqueado) {
+                String pocionDisponible = (pocion)
+                        ? "\n5 (Poción mística // 3 turnos sin atacar ni defenderte // +100% hp)"
+                        : " ";
+                System.out.println(
+                        "Con que arma quieres atacar al vampiro?\n1: (7 dmg/50% prob)\n2: (15 dmg/25% prob)\n3: (30 dmg/12% prob)\n4 (Defenderse // 0 dmg/80% prob/-5 dmg vampiro)"
+                                + pocionDisponible);
+                numeroArmaEscogida = input.nextInt();
+            }
+
+            if (numeroArmaEscogida == 5 && pocion) {
+                turnoBloqueado = true;
+                turnoActual = turno;
+                pocion = false;
+            }
+            if (turno == turnoActual + 3) {
+                turnoBloqueado = false;
+                vidaHeroe = MAX_VIDA_HEROE;
+                System.out.println("La vida del heroe se regeneró por completo!!");
+            }
 
             numeroAtaqueVampiro = (int) (Math.random() * CANTIDAD_ATAQUES_VAMPIRO) + 1;
             if (numeroAtaqueVampiro == 1) {
                 probabilidadExitoVampiro = 0.9;
                 dañoVampiro = DAÑO_VAMPIRO_ATAQUE_1;
-
             } else if (numeroAtaqueVampiro == 2) {
                 probabilidadExitoVampiro = 0.6;
                 dañoVampiro = DAÑO_VAMPIRO_ATAQUE_2;
@@ -62,7 +93,7 @@ public class reto002 {
                 dañoHeroe = DEFENSA_HEROE;
                 dañoVampiro = dañoVampiro - 5;
                 probabilidadExitoVampiro = 1;
-            } else {
+            } else if (numeroArmaEscogida != 5) {
                 System.out.println("Te equivocas e intentas pegar al vampiro con un plátano!!");
                 probabilidadExitoHeroe = 0;
                 dañoHeroe = 0;
@@ -70,23 +101,30 @@ public class reto002 {
                 probabilidadExitoVampiro = 1;
 
             }
+            if (!turnoBloqueado) {
+                if (!desmayoHeroe) {
+                    if (Math.random() < probabilidadExitoHeroe && numeroArmaEscogida != 4) {
+                        System.out.println("El vampiro se lleva un tajo!!");
+                        vidaVampiro = vidaVampiro - dañoHeroe;
+                    } else if (Math.random() < probabilidadExitoHeroe && numeroArmaEscogida == 4) {
+                        System.out.println("Te defiendes a duras penas del vampiro!!");
+                    } else if (Math.random() > probabilidadExitoHeroe && numeroArmaEscogida == 4) {
+                        System.out.println("No logras defenderte del vampiro!!");
+                    } else {
+                        System.out.println("El vampiro ha esquivado el ataque!!");
+                    }
 
-            if (!desmayoHeroe) {
-                if (Math.random() < probabilidadExitoHeroe && numeroArmaEscogida != 4) {
-                    System.out.println("El vampiro se lleva un tajo!!");
-                    vidaVampiro = vidaVampiro - dañoHeroe;
-                } else if (Math.random() < probabilidadExitoHeroe && numeroArmaEscogida == 4) {
-                    System.out.println("Te defiendes a duras penas del vampiro!!");
-                } else if (Math.random() > probabilidadExitoHeroe && numeroArmaEscogida == 4) {
-                    System.out.println("No logras defenderte del vampiro!!");
                 } else {
-                    System.out.println("El vampiro ha esquivado el ataque!!");
+                    System.out.println("El heroe está desmayado y no puede atacar!!");
                 }
-
-            }else {
-                System.out.println("El heroe está desmayado y no puede atacar!!");
+            } else {
+                System.out.println("Tu turno está bloqueado por la poción");
             }
-            if (vidaVampiro < VIDA_MINIMA_DESMAYO_VAMPIRO && vidaVampiro > 0) {
+
+            if (vidaVampiro <= 0) {
+                vampiroVivo = false;
+            }
+            if (vidaVampiro < VIDA_MINIMA_DESMAYO_VAMPIRO && vampiroVivo) {
                 desmayoVampiro = true;
                 System.out.println(
                         "El vampiro se ha desmayado del dolor!! // Al descansar ha recuperado 2 puntos de vida");
@@ -95,9 +133,6 @@ public class reto002 {
                 desmayoVampiro = false;
             }
 
-            if (vidaVampiro <= 0) {
-                vampiroVivo = false;
-            }
             if (vampiroVivo && !desmayoVampiro) {
                 if (Math.random() < probabilidadExitoVampiro) {
                     System.out.println("El heroe se lleva un mordisco!!");
@@ -105,7 +140,7 @@ public class reto002 {
                 } else {
                     System.out.println("El heroe ha esquivado el mordisco!!");
                 }
-            }else {
+            } else {
                 System.out.println("El vampiro está desamyado y no puede atacar!!");
             }
             if (vidaHeroe < VIDA_MINIMA_DESMAYO_HEROE && vidaHeroe > 0) {
@@ -127,6 +162,7 @@ public class reto002 {
                 alguienVivo = false;
             }
             System.out.println("-----------------------------");
+
         }
 
     }
