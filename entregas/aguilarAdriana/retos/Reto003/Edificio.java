2efe7a9bd6bf4b36835e954ae820ea7084a6f295
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2025-10-30 16:03:25 +0100

    Lo he vuelto a hacer mas ordenado y he agregado la cuenta de los pisos del edificio.

--- entregas/aguilarAdriana/retos/Reto003/Edificio.java
+++ entregas/aguilarAdriana/retos/Reto003/Edificio.java
@@ -1,50 +1,53 @@
 package entregas.aguilarAdriana.retos.Reto003;
 
 public class Edificio {
+
     public static void main(String[] args) {
 
         final int PLANTAS_CON_HABITACION = 7;
         final int HABITACIONES_POR_PLANTA = 6;
 
-        final double PERSIANA_ABIERTA = 0.7;
         final double LUZ_ENCENDIDA = 0.6;
+        final double PERCIANA_ABIERTA = 0.7;
 
-        final String VENTANA_CERRADA = "[ ]";
-        final String VENTANA_ABIERTA_SIN_LUZ = "[º]";
-        final String VENTANA_ABIERTA_CON_LUZ = "[*]";
-        final String SEPARADOR = "[    ]";
+        final String CERRADO = "[ ]";
+        final String APAGADO = "[º]";
+        final String ENCENDIDO = "[*]";
         final String LADRILLO = ":";
+        final String ESPACIO = "[    ]";
 
         for (int dia = 1; dia <= 10; dia++) {
-            for (int hora = 1; hora <= 24; hora++) {
+            for (int hora = 0; hora <= 24; hora++) {
                 System.out.println("               __/\\__");
-                System.out.println("  |    |    |  |####|  |    |    |  ");
+                System.out.println("  |    |    |  |####|  |    |    | ");
                 System.out.println("====================================");
-                for (int planta = 1; planta <= 7; planta++) {
-                    for (int columna = 1; columna <= 7; columna++) {
+                for (int columna = 1; columna <= PLANTAS_CON_HABITACION; columna++) {
+                    for (int planta = 1; planta <= HABITACIONES_POR_PLANTA; planta++) {
 
-                        boolean abierta, encendida;
-                        abierta = Math.random() < PERSIANA_ABIERTA;
-                        encendida = Math.random() < LUZ_ENCENDIDA;
+                        boolean abierto, encendido;
+                        abierto = Math.random() <= PERCIANA_ABIERTA;
+                        encendido = Math.random() <= LUZ_ENCENDIDA;
 
-                        if (columna == 4) {
-                            System.out.print(SEPARADOR);
-                        } else {
-                            System.out.print(LADRILLO + (!abierta ? VENTANA_CERRADA
-                                    : encendida ? VENTANA_ABIERTA_CON_LUZ : VENTANA_ABIERTA_SIN_LUZ) + LADRILLO);
+                        System.out.print(LADRILLO + (abierto ? (encendido ? ENCENDIDO : APAGADO) : CERRADO) + LADRILLO);
+                        if (planta == 3) {
+                            System.out.print(ESPACIO);
+
                         }
 
                     }
+                    int numeroPlanta = PLANTAS_CON_HABITACION - columna + 1 ;
+                    System.out.print(" - P" + numeroPlanta);
+                    
                     System.out.println();
+
                 }
                 System.out.println("------------------------------------");
                 System.out.println("     ==========================");
                 System.out.println("   ==============================");
-                System.out.println(" ==================================");
-                System.out.println("Dia " + dia + " - " + hora + ":00h");
+                System.out.println("==================================");
+                System.out.println("Dia " + dia + " --- " + hora + ":00h");
             }
         }
 
     }
-
 }
