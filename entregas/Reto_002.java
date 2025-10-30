6fe0288c85b7cec67a50fecda28e783147b118ff
boscogg07 <bosco.garcia@alumnos.uneatlantico.es>
2025-10-30 13:55:12 +0100

    Reto_003

--- entregas/Reto_002.java
+++ entregas/Reto_002.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Reto_002 {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in);
 
         int vidaGuerrero = 150;
         final int ARMA1 = 7;
@@ -13,7 +13,6 @@ public class Reto_002 {
         final int ARMA3 = 30;
         final double PROBABILIDAD_ARMA3 = 0.12;
 
-
         int vidaVampiro = 60;
         final int ATAQUE1 = 5;
         final double PROBABILIDAD_ATAQUE1 = 0.9;
@@ -21,7 +20,6 @@ public class Reto_002 {
         final double PROBABILIDAD_ATAQUE2 = 0.6;
         final int ATAQUE3 = 20;
         final double PROBABILIDAD_ATAQUE3 = 0.4;
-
 
         boolean algunMuerto = false;
         boolean guerreroVivo = true;
@@ -31,68 +29,69 @@ public class Reto_002 {
             System.out.println("¿Qué arma quieres usar?(1,2,3)");
             int arma = scanner.nextInt();
 
-            if(arma == 1){
-                if(Math.random()<PROBABILIDAD_ARMA1){
-                vidaVampiro = vidaVampiro - ARMA1;
-                System.out.println("El vampiro recibe daño del arma1");
-                }else{
-                     System.out.println("El vampiro esquiva el arma1");
+            if (arma == 1) {
+                if (Math.random() < PROBABILIDAD_ARMA1) {
+                    vidaVampiro = vidaVampiro - ARMA1;
+                    System.out.println("El vampiro recibe daño del arma1");
+                } else {
+                    System.out.println("El vampiro esquiva el arma1");
                 }
-               
-            }else if(arma==2){
-               if(Math.random()<PROBABILIDAD_ARMA2){
-                vidaVampiro = vidaVampiro - ARMA2;
-                System.out.println("El vampiro recibe daño del arma2");
-               }else{
-                System.out.println("El vampiro esquiva el arma2");
-               }
-            }else if(arma==3){
-                if(Math.random()<PROBABILIDAD_ARMA3){
+
+            } else if (arma == 2) {
+                if (Math.random() < PROBABILIDAD_ARMA2) {
+                    vidaVampiro = vidaVampiro - ARMA2;
+                    System.out.println("El vampiro recibe daño del arma2");
+                } else {
+                    System.out.println("El vampiro esquiva el arma2");
+                }
+            } else if (arma == 3) {
+                if (Math.random() < PROBABILIDAD_ARMA3) {
                     vidaVampiro = vidaVampiro - ARMA3;
                     System.out.println("El vampiro recibe daño del arma3");
-                }else{
+                } else {
                     System.out.println("El vampiro esquiva el arma3");
                 }
             }
 
-            vampiroVivo = vidaVampiro> 0;
+            vampiroVivo = vidaVampiro > 0;
 
-            if(vampiroVivo){
-                int ataque = ((int)(Math.random()*100))%3+1;
-                System.out.println("Arma elegida por el vampiro: "+ataque);
+            if (vampiroVivo) {
+                int ataque = ((int) (Math.random() * 100)) % 3 + 1;
+                System.out.println("Arma elegida por el vampiro: " + ataque);
 
-             if(ataque == 1){
-                if(Math.random()<PROBABILIDAD_ATAQUE1){
-                vidaGuerrero = vidaGuerrero - ATAQUE1;
-                System.out.println("El guerrero recibe daño del ataque1");
-                }else{
-                     System.out.println("El guerrero esquiva el ataque1");
+                if (ataque == 1) {
+                    if (Math.random() < PROBABILIDAD_ATAQUE1) {
+                        vidaGuerrero = vidaGuerrero - ATAQUE1;
+                        System.out.println("El guerrero recibe daño del ataque1");
+                    } else {
+                        System.out.println("El guerrero esquiva el ataque1");
+                    }
+
+                } else if (ataque == 2) {
+                    if (Math.random() < PROBABILIDAD_ATAQUE2) {
+                        vidaGuerrero = vidaGuerrero - ATAQUE2;
+                        System.out.println("El guerrero recibe daño del ataque2");
+                    } else {
+                        System.out.println("El guerrero esquiva el ataque2");
+                    }
+                } else if (ataque == 3) {
+                    if (Math.random() < PROBABILIDAD_ATAQUE3) {
+                        vidaGuerrero = vidaGuerrero - ATAQUE3;
+                        System.out.println("El guerrero recibe daño del ataque3");
+                    } else {
+                        System.out.println("El guerrero esquiva el ataque3");
+                    }
                 }
-               
-            }else if(ataque==2){
-               if(Math.random()<PROBABILIDAD_ATAQUE2){
-                vidaGuerrero = vidaGuerrero - ATAQUE2;
-                System.out.println("El guerrero recibe daño del ataque2");
-               }else{
-                System.out.println("El guerrero esquiva el ataque2");
-               }
-            }else if(ataque==3){
-                if(Math.random()<PROBABILIDAD_ATAQUE3){
-                    vidaGuerrero = vidaGuerrero - ATAQUE3;
-                    System.out.println("El guerrero recibe daño del ataque3");
-                }else{
-                    System.out.println("El guerrero esquiva el ataque3");
-                }
-            }
             }
             System.out.println("Vida Guerrero [" + vidaGuerrero + "]/Vida Vampiro[" + vidaVampiro + "]");
 
-            guerreroVivo = vidaGuerrero>0;
+            guerreroVivo = vidaGuerrero > 0;
 
             algunMuerto = !guerreroVivo || !vampiroVivo;
 
-        }while(!algunMuerto);
+        } while (!algunMuerto);
 
-    String elGanador = vidaGuerrero > 0 ? "Guerrero" : "Vampiro";System.out.println("Ganó el "+elGanador);
-}
+        String elGanador = vidaGuerrero > 0 ? "Guerrero" : "Vampiro";
+        System.out.println("Ganó el " + elGanador);
+    }
 }
---
+++ entregas/Reto_003.java
@@ -0,0 +1,41 @@
+
+public class Reto_003 {
+    public static void main(String[] args) {
+
+        final double PERSIANA_ABIERTA = 0.7;
+        final double LUZ_ENCENDIDA = 0.6;
+
+        boolean abierta, encendida;
+
+        final String TEJADO_UNO = "               __/\\__";
+        final String TEJADO_DOS = "  |    |    |  |####|  |    |    |  ";
+        final String TEJADO_TRES = "====================================";
+        final String SUELO = "------------------------------------";
+        final String SEPARADOR = ":[    ]:";
+        final String VENTANA_CERRADA = ":[ ]:";
+        final String VENTANA_ABIERTA_SIN_LUZ = ":[º]:";
+        final String VENTANA_ABIERTA_CON_LUZ = ":[*]:";
+
+        for (int dia = 1; dia < 7; dia++) {
+            for (int hora = 0; hora < 24; hora++) {
+                System.out.println("Son las " + hora + " del día " + dia);  
+                System.out.println(TEJADO_UNO);
+                System.out.println(TEJADO_DOS);
+                System.out.println(TEJADO_TRES);                      
+                for (int planta = 1; planta <= 7; planta++) {
+                    for (int columna = 1; columna <= 6; columna++) {
+                        abierta = Math.random() < PERSIANA_ABIERTA;
+                        encendida = Math.random() < LUZ_ENCENDIDA; 
+                              
+                        System.out.print(!abierta ? VENTANA_CERRADA : encendida ? VENTANA_ABIERTA_CON_LUZ : VENTANA_ABIERTA_SIN_LUZ);
+                        if (columna == 3) {
+                            System.out.print(SEPARADOR);
+                        }
+                    }
+                    System.out.println();
+                } 
+            }
+           
+        }
+    }
+}
