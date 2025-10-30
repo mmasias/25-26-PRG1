800caa907f89e594d366a3e24ace25b5ca7cc251
marcosacevedo-gif <marcos.acevedo@uneatlantico.es>
2025-10-30 14:30:27 +0100

    reto-003

--- entregas/AcevedoMarcos/Reto-003/edificio.java
+++ entregas/AcevedoMarcos/Reto-003/edificio.java
@@ -1,19 +1,40 @@
 import java.util.Scanner;
 
-public class edificiohotel {
+class Edificio {
+
     public static void main(String[] args) {
-    final double persianaabierta = 0.7;
-    final double luzencendida = 0.6;
 
-    final String ventana_cerrada= "[]";
-    final String ventana_abierta_con_luz= "[*]";
-    final String ventana_abierta_sin_luz= "[º]";
+        final double PERSIANA_ABIERTA = 0.7;
+        final double LUZ_ENCENCIDA = 0.6;
 
-    boolean abierta, encendida; 
-    for (int j=1; j<=7; j++) {
-         abierta = Math.random() < persianaabierta;
-    encendida = Math.random() < luzencendida;
-    System.out.print("abierta ? ventana_cerrada : ventana_abierta_con_luz : ventana_abierta_sin_luz");
+        final String TECHO_ESPACIO_ARRIBA = "              __/\\__               ";
+        final String TECHO_ESPACIO_MEDIO = "  |    |    |  |####|  |    |    |  ";
+        final String TECHO_ESPACIO_BAJO = " ==================================== ";
+        final String BASE = "------------------------------------ ";
+        final String VENTANA_CERRADA = "[ ]";
+        final String VENTANA_ABIERTA_CON_LUZ = "[*]";
+        final String VENTANA_ABIERTA_SIN_LUZ = "[º]";
+        final String SEPARADOR = " [    ] ";
+
+        boolean abierta, encendida;
+        for (int dia=1; dia<=7;dia++) {
+            for (int hora=0; hora<=24; hora++) {
+                System.out.println(TECHO_ESPACIO_ARRIBA);
+                System.out.println(TECHO_ESPACIO_MEDIO);
+                System.out.println(TECHO_ESPACIO_BAJO);
+                System.out.println("Son las " +  hora + " del dia " + dia);
+                for (int planta=1; planta<=7; planta++) {
+                    for (int columna=1; columna<=6; columna++) {
+                        abierta = Math.random() < PERSIANA_ABIERTA;
+                        encendida = Math.random() < LUZ_ENCENCIDA;
+                        System.out.println(!abierta ? VENTANA_CERRADA : encendida ? VENTANA_ABIERTA_CON_LUZ : VENTANA_ABIERTA_SIN_LUZ);
+                        if (columna == 3 ) {
+                            System.out.print( SEPARADOR );
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(BASE);
     }
-}
 }
