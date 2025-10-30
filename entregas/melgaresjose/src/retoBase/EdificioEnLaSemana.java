44bde2126eb73d64794b777581a86cb10b09676e
J Melg <rjmzelaya@gmail.com>
2025-10-30 13:29:53 +0100

    Dibujado edificio a traves de la semana

--- entregas/melgaresjose/src/retoBase/EdificioEnLaSemana.java
+++ entregas/melgaresjose/src/retoBase/EdificioEnLaSemana.java
@@ -6,21 +6,35 @@ class EdificioEnLaSemana {
         final double PROBABILIDAD_LUZ_ENCENDIDA = 0.60;
         final int PLANTAS_HABITACIONES = 7;
         final int HABITACIONES_POR_PLANTA = 6;
-        final String LUZ_APAGADA = "º";
-        final String LUZ_ENCENDIDA = "*";
+        final String LUZ_APAGADA = ":[º]:";
+        final String LUZ_ENCENDIDA = ":[*]:";
+        String PERSIANA_CERRADA = ":[ ]:";
         boolean estaAbierta = false;
         boolean luzEstaEncendida = false;
         int numeroDia;
         int numeroHora;
         for(int i = 1; i < DIAS_EN_SEMANA*HORAS_EN_DIA; i++) {
-            for(int j = 1; j <= HABITACIONES_POR_PLANTA; j++) {
-                
+            System.out.println(" ______________//^\\\\_____________");
+            System.out.println("/   |   |   | |[###]| |   |   |   \\");
+            System.out.println("===================================");
+            for(int planta = PLANTAS_HABITACIONES; planta>= 1; planta-- ){
+                for(int habitacion = 1; habitacion <= HABITACIONES_POR_PLANTA; habitacion++) {
+                    estaAbierta = Math.random() < PROBABILIDAD_ABIERTA;
+                    luzEstaEncendida = Math.random() < PROBABILIDAD_LUZ_ENCENDIDA;
+                    if(!estaAbierta)
+                        System.out.print(PERSIANA_CERRADA);
+                    else                     
+                        System.out.print(luzEstaEncendida ? LUZ_ENCENDIDA : LUZ_APAGADA);
+                    System.out.print(habitacion == 3 ? "[   ]": "");
+                }
+                System.out.print(" - Planta " + planta);
+                System.out.println();
             }
-            
             
             numeroDia = (int) (i / HORAS_EN_DIA) + 1;
             numeroHora = (i % HORAS_EN_DIA);
             System.out.println("Dia: " + numeroDia + " hora: " + numeroHora+ ":00");
+            System.out.println();
         }
     }
 }
