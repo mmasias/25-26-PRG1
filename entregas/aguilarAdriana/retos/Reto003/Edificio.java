9e7d8aab2e7f3ede273f5f0fc180c72347bd2211
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2025-10-30 13:55:16 +0100

    reto003Formateado

--- entregas/aguilarAdriana/retos/Reto003/Edificio.java
+++ entregas/aguilarAdriana/retos/Reto003/Edificio.java
@@ -15,16 +15,14 @@ public class Edificio {
         final String SEPARADOR = "[    ]";
         final String LADRILLO = ":";
 
-       
-
         for (int dia = 1; dia <= 10; dia++) {
             for (int hora = 1; hora <= 24; hora++) {
-                 System.out.println("               __/\\__");
-        System.out.println("  |    |    |  |####|  |    |    |  ");
-        System.out.println("====================================");
+                System.out.println("               __/\\__");
+                System.out.println("  |    |    |  |####|  |    |    |  ");
+                System.out.println("====================================");
                 for (int planta = 1; planta <= 7; planta++) {
                     for (int columna = 1; columna <= 7; columna++) {
-                        
+
                         boolean abierta, encendida;
                         abierta = Math.random() < PERSIANA_ABIERTA;
                         encendida = Math.random() < LUZ_ENCENDIDA;
@@ -32,22 +30,20 @@ public class Edificio {
                         if (columna == 4) {
                             System.out.print(SEPARADOR);
                         } else {
-                            System.out.print(LADRILLO+(!abierta ? VENTANA_CERRADA
-                                    : encendida ? VENTANA_ABIERTA_CON_LUZ : VENTANA_ABIERTA_SIN_LUZ)+LADRILLO);
+                            System.out.print(LADRILLO + (!abierta ? VENTANA_CERRADA
+                                    : encendida ? VENTANA_ABIERTA_CON_LUZ : VENTANA_ABIERTA_SIN_LUZ) + LADRILLO);
                         }
 
                     }
                     System.out.println();
                 }
-                 System.out.println("------------------------------------");
-        System.out.println("     ==========================");
-        System.out.println("   ==============================");
-        System.out.println(" ==================================");
+                System.out.println("------------------------------------");
+                System.out.println("     ==========================");
+                System.out.println("   ==============================");
+                System.out.println(" ==================================");
                 System.out.println("Dia " + dia + " - " + hora + ":00h");
             }
         }
-
-       
 
     }
 
