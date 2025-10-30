443b2d56ef21bb5666ee61496d90a6d68ad2042f
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-10-30 13:05:25 +0100

    Techo y base

---
+++ entregas/sanmiguelHector/reto-003/DibujadorDeEdificio.java
@@ -0,0 +1,11 @@
+public class DibujadorDeEdificio {
+    public static void main(String[] args) {
+        final String TECHO_EDIFICIO =  "               __/\\__\n" +
+                "  |    |    |  |####|  |    |    |\n" +
+                "====================================\n";
+        final String BASE = "------------------------------------";
+
+        System.out.println(TECHO_EDIFICIO);
+        System.out.println(BASE);
+    }
+}
