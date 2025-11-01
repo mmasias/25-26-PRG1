315e38cff9b4f2bb5532f5736e65befb4c96d83e
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-11-01 23:52:26 +0100

    Ejercicio base

--- entregas/sanmiguelHector/ejercicios/UnPozo.java
+++ entregas/sanmiguelHector/ejercicios/UnPozo.java
@@ -1,21 +1,31 @@
+package entregas.sanmiguelHector.ejercicios;
+
 public class UnPozo {
     public static void main(String[] args) {
 
-        final String PARTE_ARRIBA_POZO = "[__]              [__]";
+        final String PARTE_ARRIBA_POZO = "[__]               [__]";
         final String LATERAL_POZO = "[]";
-        final String AGUA = ":. ";
+        final String AGUA = ":.";
+        final String BASE_POZO = "  [][][][][][][][][] ";
         final int PROFUNDIDAD_POZO = 10;
-        final int ANCHURA_POZO = 5;
+        final int ANCHURA_POZO = 7;
+        int fila;
+        int columna;
 
-        System.out.print(PARTE_ARRIBA_POZO);
-        for (int columna = 1; columna <= PROFUNDIDAD_POZO; columna++) {
-            for (int fila = 1; fila <= ANCHURA_POZO; fila++){
-                if (fila == 1 || fila == ANCHURA_POZO) {
-                    System.out.print(LATERAL_POZO);
+        System.out.println(PARTE_ARRIBA_POZO);
+        for (fila = 1; fila <= PROFUNDIDAD_POZO; fila++) {
+            for (columna = 1; columna <= ANCHURA_POZO; columna++) {
+                if (columna == 1) {
+                    System.out.print("  " + LATERAL_POZO);
                 }
                 System.out.print(AGUA);
+                if (columna == ANCHURA_POZO) {
+                    System.out.print(LATERAL_POZO);
+                }
             }
+            System.out.print(" _ __ " + fila);
             System.out.println();
         }
+        System.out.println(BASE_POZO);
     }
 }
