a510e4059c26ba0c184464fa1f8b5bcb3d86ecdb
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-11-02 12:55:06 +0100

    Reto 2

--- entregas/sanmiguelHector/ejercicios/RetoForIf.java
+++ entregas/sanmiguelHector/ejercicios/RetoForIf.java
@@ -10,7 +10,7 @@ class RetoForIf {
 
                 if (
                 // El código desde aqui *********************************
-                    i==2 && j==2
+                    i==2
                 // El código hasta aquí *********************************
                 ) {
                     System.out.print("(*)");
