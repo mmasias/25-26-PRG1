29e8c8a72ac8414c8c90e7087c27bc32434b6927
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-11-02 17:36:40 +0100

    Whac-a-mole extendido

--- entregas/sanmiguelHector/ejercicios/WhacaMole.java
+++ entregas/sanmiguelHector/ejercicios/WhacaMole.java
@@ -9,13 +9,24 @@ public class WhacaMole {
         final String AGUJERO_CON_MONIGOTE = "('')";
         final String AGUJERO_GOLPEADO = "([])";
         final String AGUJERO_CON_MONIGOTE_GOLPEADO = "[**]";
+        final String MENSAJE_VICTORIA = "Golpeaste a un topo!";
+        final String MENSAJE_DERROTA = "No le diste a nadie";
 
         Random random = new Random();
 
-        int filaPensadaPorOrdenador = random.nextInt(4) + 1;
-        int columnaPensadaPorOrdenador = random.nextInt(4) + 1;
+        int filaTopo1 = random.nextInt(4) + 1;
+        int columnaTopo1 = random.nextInt(4) + 1;
+        int filaTopo2 = random.nextInt(4) + 1;
+        int columnaTopo2 = random.nextInt(4) + 1;
+
+        while (filaTopo1 == filaTopo2 && columnaTopo1 == columnaTopo2) {
+            filaTopo2 = random.nextInt(4) + 1;
+            columnaTopo2 = random.nextInt(4) + 1;
+        }
 
         Scanner scanner = new Scanner(System.in);
+
+        boolean topoGolpeado = false;
 
         System.out.println("¿Qué fila quieres golpear? (1-4)");
         int filaSeleccionada = scanner.nextInt();
@@ -27,9 +38,10 @@ public class WhacaMole {
         for (int fila = 1; fila <= 4; fila++) {
             for (int columna = 1; columna <= 4; columna++) {
 
-                if (fila == filaPensadaPorOrdenador && columna == columnaPensadaPorOrdenador) {
+                if ((fila == filaTopo1 && columna == columnaTopo1) || (fila == filaTopo2 && columna == columnaTopo2)) {
                     if (fila == filaSeleccionada && columna == columnaSeleccionada) {
                         System.out.print(AGUJERO_CON_MONIGOTE_GOLPEADO);
+                        topoGolpeado = true;
                     } else {
                         System.out.print(AGUJERO_CON_MONIGOTE);
                     }
@@ -43,6 +55,12 @@ public class WhacaMole {
 
             }
             System.out.println();
+        }
+        
+        if (topoGolpeado){
+            System.out.println(MENSAJE_VICTORIA);
+        } else {
+            System.out.print(MENSAJE_DERROTA);
         }
         scanner.close();
     }
