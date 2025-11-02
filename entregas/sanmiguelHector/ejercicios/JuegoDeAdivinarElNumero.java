dc22cffeda30f2a2a7e506373ac2a5cbac6cd500
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-11-02 12:39:48 +0100

    Errores corregidos

--- entregas/sanmiguelHector/ejercicios/JuegoDeAdivinarElNumero.java
+++ entregas/sanmiguelHector/ejercicios/JuegoDeAdivinarElNumero.java
@@ -1,3 +1,4 @@
+package entregas.sanmiguelHector.ejercicios;
 import java.util.Random;
 import java.util.Scanner;
 
--- entregas/sanmiguelHector/ejercicios/PiedraPapelTijeras.java
+++ entregas/sanmiguelHector/ejercicios/PiedraPapelTijeras.java
@@ -1,3 +1,4 @@
+package entregas.sanmiguelHector.ejercicios;
 import java.util.Scanner;
 import java.util.Random;
 
