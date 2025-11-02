5a8fb5f958d753b7016c982486734809e100a058
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2025-11-02 13:39:58 +0100

    Reto 16

--- entregas/sanmiguelHector/ejercicios/RetoForIf.java
+++ entregas/sanmiguelHector/ejercicios/RetoForIf.java
@@ -10,7 +10,7 @@ class RetoForIf {
 
                 if (
                 // El código desde aqui *********************************
-                    (i > 1 && i < 6) || (j > 1 && j < 6)
+                    (i > 1 && i < 21) && (j > 1 && j < 21)
                 // El código hasta aquí *********************************
                 ) {
                     System.out.print("(*)");
