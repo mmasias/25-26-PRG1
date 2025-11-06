c8677174e54f7e1ff239e77a7f5376eb25bccabf
Carlos <carlos.rodriguez1@alumnos.uneatlantico.es>
2025-11-06 13:26:09 +0100

    Subo el reto 4

---
+++ entregas/rodriguezCarlos/retos/CarreraDeCamellos.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class CarreraDeCamellos {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int turno = 0;
+        int casilla = 0;
+        int avance = 0;
+        boolean acierto = false;
+
+        while (casilla < 60) {
+            System.out.println("Turno " + turno);
+            System.out.println("Elija un agujero:");
+            int agujero = scanner.nextInt();
+
+            if (agujero < 1 || agujero > 4) {
+                System.out.println("Agujero no válido.");
+                continue;
+            }
+
+            double probabilidad = Math.random();
+            acierto = false;
+            avance = 0;
+
+            if (agujero == 1) {
+                if (probabilidad <= 0.60) {
+                    acierto = true;
+                    avance = 1;
+                }
+            } else if (agujero == 2) {
+                if (probabilidad <= 0.40) {
+                    acierto = true;
+                    avance = 2;
+                }
+            } else if (agujero == 3) {
+                if (probabilidad <= 0.30) {
+                    acierto = true;
+                    avance = 4;
+                }
+            } else if (agujero == 4) {
+                if (probabilidad <= 0.10) {
+                    acierto = true;
+                    avance = 6;
+                }
+            }
+
+            if (acierto) {
+                casilla += avance;
+                if (casilla > 60) {
+                    casilla = 60;
+                }
+                System.out.println("Turno " + turno + " - Avanza " + avance + " casillas - Esta en la casilla " + casilla);
+            } else {
+                System.out.println("Turno " + turno + " - No avanza ninguna casilla - Esta en la casilla " + casilla);
+            }
+
+            turno++;
+        }
+        
+        System.out.println("El juego ha terminado en la casilla 60");
+        scanner.close();
+    }
+}
