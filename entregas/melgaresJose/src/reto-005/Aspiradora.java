7a279ebae1f44831cbd92d0b135e679bd8fc13bb
J Melg <rjmzelaya@gmail.com>
2025-12-02 13:38:24 +0100

    feat: maps surface

--- entregas/melgaresJose/src/reto-005/Aspiradora.java
+++ entregas/melgaresJose/src/reto-005/Aspiradora.java
@@ -1,20 +1,13 @@
 class Aspiradora {
     public static void main(String[] args) {
-        String[] estadosSuciedad = {
-            " . ",
-            "...",
-            "ooo",
-            "OOO",
-            "***"
-        };
         int[] coordAspiradora = {0, 0};
         
         int[][] superficie = crearSuperficie(5,8);
-        dibujarSuperficie(superficie, estadosSuciedad);    
+        dibujarSuperficie(superficie, coordAspiradora);    
     }
 
-    private static int[][] crearSuperficie(int dimVertical, int dimHorizontal) {
-        int[][] superficie = new int[dimVertical][dimHorizontal];
+    private static int[][] crearSuperficie(int dimY, int dimX) {
+        int[][] superficie = new int[dimY][dimX];
         for (int i = 0; i < 16;i++) {
             int x = (int) (Math.random() * superficie.length);
             int y = (int)(Math.random() * superficie[0].length);
@@ -23,21 +16,36 @@ class Aspiradora {
         return superficie;
     }
 
-    private static void dibujarSuperficie(int[][] superficie, String[] suciedades) {
-        int posVertical = superficie.length;
-        int posHorizontal = superficie[posVertical - 1].length;  
-        
+    private static void dibujarSuperficie(int[][] superficie, int[] coordAspiradora) {
+        moverAspiradora(coordAspiradora);
         for(int i = 0; i< superficie.length; i++) {
             for(int j = 0;j < superficie[i].length; j++) {
-                if(i == posVertical && j == posHorizontal) {
-                    System.out.println("(0)");
+                
+                if(i == coordAspiradora[1] && j == coordAspiradora[0]) {
+                    System.out.print("(Q)");
                 }
-                 else {
-                    System.out.print(suciedades[superficie[i][j]]);
-                 }
-
+                else {
+                    System.out.print(mapear(superficie[i][j]));
+                }
             }
             System.out.println();
+            
         }
+    }
+
+    private static void moverAspiradora(int[] coordenadas) {
+        coordenadas[0] = 3;
+        coordenadas[1] = 2;
+    }
+
+    private static String mapear(int tile) {
+        String[] estadosSuciedad = {
+            " . ",
+            "...",
+            "ooo",
+            "OOO",
+            "***"
+        };
+        return estadosSuciedad[tile];
     }
 }
