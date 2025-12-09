d7d69df0d37e073a6d65fcec03d82c717d2a2768
javierrodriguezrobles07-creator <javierrodriguezrobles07@gmail.com>
2025-12-09 13:33:44 +0100

    commit

--- entregas/javierrodriguezrobles07-creator/Pozo.java
+++ entregas/javierrodriguezrobles07-creator/Pozo.java
@@ -1,26 +1,3 @@
 public class Pozo {
-    public static void main(String[] args) {
-
-        final int PROFUNDIDAD_POZO = 20;
-        final String BORDE_SUPERIOR = "[__]              [__]";
-        final String PARED = ""; 
-        final String BORDE_INFERIOR = "";
-        final String SEPARADOR = ""; 
-        final int PROFUNDIDAD_AGUA = 3;
-        final String AGUA = "" ; 
-        final int PROFUNDIDAD_CARACOL = 8;
-        final String CARACOL = "" ; 
-
-        System.out.println(BORDE_SUPERIOR);
-        for(int profundidad = 0; profundidad <= PROFUNDIDAD_POZO; profundidad = profundidad + 1) {
-
-            if (profundidad >= PROFUNDIDAD_POZO - PROFUNDIDAD_AGUA) {
-                System.out.println(AGUA);
-            } else {
-                System.out.println(PARED);  
-            }
-            System.out.println(SEPARADOR + profundidad);
-
-        }
-    } 
+    pu
 }
