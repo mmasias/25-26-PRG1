43036666146ef33ff20a5f81573e79724dcf025f
J Melg <rjmzelaya@gmail.com>
2025-12-09 13:30:57 +0100

    feat: adds gato vs aspiradora

--- entregas/melgaresJose/src/reto-005/Aspiradora.java
+++ entregas/melgaresJose/src/reto-005/Aspiradora.java
@@ -1,51 +1,151 @@
-class Aspiradora {
+import java.util.Scanner;
+
+public class AspiradoraSolClase {
+
     public static void main(String[] args) {
-        int[] coordAspiradora = {0, 0};
-        
-        int[][] superficie = crearSuperficie(5,8);
-        dibujarSuperficie(superficie, coordAspiradora);    
+
+        int[][] superficie = {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        int[] posicionAspiradora = { 4, 4 };
+        int[] posicionGato = {3,3};
+        ensuciarEscenario(superficie);
+        boolean hayQueLimpiar = estaSucia(superficie);;
+
+        do {
+            moverObjeto(posicionGato, superficie);
+            moverObjeto(posicionAspiradora, superficie);
+            imprimirEscenario(superficie, posicionAspiradora, posicionGato);
+            limpiar(superficie, posicionAspiradora);
+            ensuciar(superficie, posicionGato);
+            hayQueLimpiar = estaSucia(superficie);
+            new Scanner(System.in).nextLine();
+        } while (hayQueLimpiar);
     }
 
-    private static int[][] crearSuperficie(int dimY, int dimX) {
-        int[][] superficie = new int[dimY][dimX];
-        for (int i = 0; i < 16;i++) {
-            int x = (int) (Math.random() * superficie.length);
-            int y = (int)(Math.random() * superficie[0].length);
-            superficie[x][y] = (int)(Math.random() * 4) + 1;
+    static boolean estaSucia(int[][] superficie){
+        for (int i = 0; i < superficie.length; i++) {
+            for (int j = 0; j < superficie[i].length; j++) {
+                 if(superficie[i][j]>0){
+                     return true;   
+                 }
+            }
         }
-        return superficie;
+        return false;
     }
 
-    private static void dibujarSuperficie(int[][] superficie, int[] coordAspiradora) {
-        moverAspiradora(coordAspiradora);
-        for(int i = 0; i< superficie.length; i++) {
-            for(int j = 0;j < superficie[i].length; j++) {
-                
-                if(i == coordAspiradora[1] && j == coordAspiradora[0]) {
-                    System.out.print("(Q)");
+    static void limpiar(int[][] superficie, int[] aspiradora) {
+
+        if (estaSucio(superficie[aspiradora[0]][aspiradora[1]])) {
+            superficie[aspiradora[0]][aspiradora[1]] = superficie[aspiradora[0]][aspiradora[1]] - 1;
+        }
+    }
+
+    static void ensuciar(int[][] superficie, int[] gato) {
+        if (!estaMaximoSucio(superficie[gato[0]][gato[1]])) {
+            superficie[gato[0]][gato[1]] = superficie[gato[0]][gato[1]] + 1;
+        }
+    }
+
+    static boolean estaSucio(int tile) {
+        return tile > 0;
+    }
+    static boolean estaMaximoSucio(int tile) {
+        return tile ==4;
+    } 
+    
+
+    static void moverObjeto(int[] posicionObjeto, int[][] superficie) {
+        final int X = 1;
+        final int Y = 0;
+
+        int movimientoX = (int) (Math.random() * 3) - 1;
+        int movimientoY = (int) (Math.random() * 3) - 1;
+
+        posicionObjeto[X] = posicionObjeto[X] + movimientoX;
+        posicionObjeto[Y] = posicionObjeto[Y] + movimientoY;
+
+        if (!posicionValida(posicionObjeto, superficie)) {
+            posicionObjeto[X] = posicionObjeto[X] - movimientoX;
+            posicionObjeto[Y] = posicionObjeto[Y] - movimientoY;
+        }
+    }
+
+    static boolean posicionValida(int[] posicionAlgo, int[][] superficie) {
+        return posicionAlgo[0] >= 0 && posicionAlgo[0] < superficie.length
+                && posicionAlgo[1] >= 0 && posicionAlgo[1] < superficie[0].length;
+    }
+
+    static void ensuciarEscenario(int[][] superficie) {
+
+        for (int row = 0; row < superficie.length; row++) {
+            for (int column = 0; column < superficie[row].length; column++) {
+                superficie[row][column] = (int) (Math.random() * 5);
+            }
+        }
+    }
+
+    static void imprimirEscenario(int[][] superficie, int[] posicionAspiradora, int[] posicionGato) {
+        String elBorde = imprimeBordeHorizontal(superficie[0].length);
+        System.out.println(elBorde);
+
+        for (int row = 0; row < superficie.length; row++) {
+            System.out.print("|");
+            for (int column = 0; column < superficie[row].length; column++) {
+
+                if (row == posicionAspiradora[0] && column == posicionAspiradora[1]) {
+                    if(posicionAspiradora[0] == posicionGato[0] && posicionAspiradora[1] == posicionGato[1] ){
+                        System.out.print(mapearGatoEnAspiradora());
+                    }
+                    else {
+                        System.out.print(mapearAspiradora());
+                    }
+                }
+                else if (row == posicionGato[0] && column == posicionGato[1]){
+                    System.out.print(mapearGato());
                 }
                 else {
-                    System.out.print(mapear(superficie[i][j]));
+                    int elTile = superficie[row][column];
+                    System.out.print(mapear(elTile));
                 }
             }
-            System.out.println();
-            
+            System.out.println("|");
         }
+        System.out.println(elBorde);
     }
 
-    private static void moverAspiradora(int[] coordenadas) {
-        coordenadas[0] = 3;
-        coordenadas[1] = 2;
+    static String imprimeBordeHorizontal(int length) {
+        return "+" + "===".repeat(length) + "+";
     }
 
-    private static String mapear(int tile) {
+    static String mapearAspiradora() {
+        return "(O)";
+    }
+
+    static String mapearGato() {
+        return "^3^";
+    }
+    static String mapearGatoEnAspiradora() {
+        return "(3)";
+    }
+    
+    static String mapear(int tile) {
+
         String[] estadosSuciedad = {
-            " . ",
-            "...",
-            "ooo",
-            "OOO",
-            "***"
+                " . ",
+                "...",
+                "ooo",
+                "OOO",
+                "***"
         };
         return estadosSuciedad[tile];
     }
+
 }
