420515d81725939d0a4e9a684084c19310bfe570
Silviamontes43 <silvia.montes@alumnos.uneatlantico.es>
2025-12-09 13:43:44 +0100

    reto-005

---
+++ ejercicios/Matrices.java
@@ -0,0 +1,15 @@
+class Matrices {
+    public static void main (String [] args) {
+        int [] numeros= {1,2,3,4};
+        double [] numerosDobles= {0.3,5.23,6.675,9};
+
+        String [] asignaturas = {"Programacion", "tecnologia y estructura de datos, matematicas"};
+        int [] otrosNumeros = {4,5,3,5,6,4};
+        otrosNumeros = new int [6];
+        boolean [] haGanado;
+        haGanado= new boolean [6];
+
+
+
+    }
+}
--- entregas/MontesSilvia/reto-003/UnEdificio.java
+++
@@ -1,6 +0,0 @@
-package src; 
-class UnEdificio{
-    public static void main (String []args) {
-        
-    }
-}
